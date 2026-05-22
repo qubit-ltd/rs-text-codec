7de350fe2fe901dadeed081bfd2c0964a15498ed
Haixing Hu <starfish.hu@gmail.com>
2026-05-22 08:40:27 +0800

    chore(io): remove moved files from legacy paths

--- tests/codec/coder_status_tests.rs
+++
@@ -1,7 +0,0 @@
-use qubit_text_codec::CoderStatus;
-
-#[test]
-fn test_coder_status_variants_are_distinct() {
-    assert_ne!(CoderStatus::Complete, CoderStatus::NeedInput);
-    assert_ne!(CoderStatus::NeedInput, CoderStatus::NeedOutput);
-}
